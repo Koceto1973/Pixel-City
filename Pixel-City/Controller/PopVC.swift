95a29536b26f5802ada352cba0336dc880b9c5fa
Koceto1973 <koceto1973@yahoo.ca>
2018-11-12 21:30:00 -0800

    downloaded images for location are popping up

---
+++ Pixel-City/Controller/PopVC.swift
@@ -0,0 +1,38 @@
+//
+//  PopVC.swift
+//  Pixel-City
+//
+//  Created by K.K. on 12.11.18.
+//  Copyright © 2018 K.K. All rights reserved.
+//
+
+import UIKit
+
+class PopVC: UIViewController, UIGestureRecognizerDelegate {
+    
+    @IBOutlet weak var popImageView: UIImageView!
+    
+    var passedImage: UIImage!
+    
+    func initData(forImage image: UIImage) {
+        self.passedImage = image
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        popImageView.image = passedImage
+        addDoubleTap()
+    }
+    
+    func addDoubleTap() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(screenWasDoubleTapped))
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.delegate = self
+        view.addGestureRecognizer(doubleTap)
+    }
+    
+    @objc func screenWasDoubleTapped() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
